8ee598222d86b6ec75003f23bffc89a9b642ea4d
Arman <arman.cs.bd@gmail.com>
2018-01-07 23:12:04 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('#ar_dots').delay(1600).fadeOut(300,function(){
 		$('#ar_bg_left').animate({left:'-50%'},300);
-		$('#ar_bg_right').animate({left:'-50%'},300,function(){
+		$('#ar_bg_right').animate({right:'-50%'},300,function(){
 			$('#ar_preloader').fadeOut(10);
 		});
 	});
