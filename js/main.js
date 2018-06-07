4ba476e6cdf1e6cd0c885b8e1a895da4cb6662cb
Arman <arman.cs.bd@gmail.com>
2018-06-07 07:04:59 +0600

    work-filed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 // Preloader fadeout and fadein
 $(document).ready(function(){
-	$('#ar_dots').delay(1800).fadeOut(300,function(){
-		$('#ar_bg_left').animate({left:'-50%'},300);
-		$('#ar_bg_right').animate({right:'-50%'},300,function(){
-			$('#ar_preloader').fadeOut(10);
+	$('#ar_dots').delay(3000).fadeOut(300,function(){
+		$('#ar_bg_left').animate({left:'-50%'},600);
+		$('#ar_bg_right').animate({right:'-50%'},600,function(){
+			$('#ar_preloader').fadeOut(20);
 		});
 	});
 });
